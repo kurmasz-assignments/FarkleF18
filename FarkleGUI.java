193e8cfdb945cb86092d97368c1eb7923ec9d978
Zachary Kurmas <kurmasz@users.noreply.github.com>
2018-10-20 11:03:18 -0400

    Initial commit

---
+++ FarkleGUI.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class FarkleGUI here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FarkleGUI
+{
+   private FarkleStub game;;
+    
+   public static void main(String[] args) {
+     new FarkleGUI();  
+    }
+}
